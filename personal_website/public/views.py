11170f5bdc5ae84a6ad4d2822f66b568b38517b6
Ryan Wellborn <wellborn.ryan@gmail.com>
2017-07-10 19:37:33 -0600

    Final Touchups. TODO: Prep for deployment.

--- personal_website/public/views.py
+++ personal_website/public/views.py
@@ -24,7 +24,7 @@ def home():
     return render_template('public/home.html')
 
 
-blueprint.route('/login/', methods=['GET','POST'])
+@blueprint.route('/login/', methods=['GET', 'POST'])
 def login():
     """Login."""
     form = LoginForm(request.form)
@@ -47,19 +47,6 @@ def logout():
     logout_user()
     flash('You are logged out.', 'info')
     return redirect(url_for('public.home'))
-
-
-@blueprint.route('/register/', methods=['GET', 'POST'])
-def register():
-    """Register new user."""
-    form = RegisterForm(request.form)
-    if form.validate_on_submit():
-        User.create(username=form.username.data, email=form.email.data, password=form.password.data, active=True)
-        flash('Thank you for registering. You can now log in.', 'success')
-        return redirect(url_for('public.home'))
-    else:
-        flash_errors(form)
-    return render_template('public/register.html', form=form)
 
 
 @blueprint.route('/about/')
